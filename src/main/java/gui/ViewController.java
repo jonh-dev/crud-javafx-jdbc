c1f100816c672f10e11f763bc40c73e8b7dd0b2d
jonh-dev <jonh.dev.br@gmail.com>
2022-08-15 17:10:33 -0300

    Local project created

---
+++ src/main/java/gui/ViewController.java
@@ -0,0 +1,14 @@
+package gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class ViewController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,17 @@
+module application.crudjavafxjdbc {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    exports application;
+    opens application to javafx.fxml;
+    exports gui;
+    opens gui to javafx.fxml;
+}
