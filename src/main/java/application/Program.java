aec6b3daed3ab659d62e1edb3ba82aeb0162f155
jonh-dev <jonh.dev.br@gmail.com>
2022-08-15 17:16:45 -0300

    Main View

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -8,13 +8,14 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class Program extends Application {
+
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Program.class.getResource("/gui/View.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Program.class.getResource("/gui/MainView.fxml"));
+        Scene mainScene = new Scene(fxmlLoader.load(), 320, 240);
+        primaryStage.setTitle("Hello!");
+        primaryStage.setScene(mainScene);
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
